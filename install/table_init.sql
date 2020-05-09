db41954f2c22fe7c0dd3365cc5600acb58636293
Pegah <pegsoltani@gmail.com>
2020-05-09 19:06:14 +0300

    Create candidate table

--- install/table_init.sql
+++ install/table_init.sql
@@ -10,3 +10,13 @@ CREATE TABLE admin(
 	adminID int PRIMARY KEY,
 	FOREIGN KEY (adminID) REFERENCES user(userID)
 );
+CREATE TABLE candidate(
+	candidateID INT PRIMARY KEY,
+	cvfile BLOB,
+	gpa DOUBLE,
+	graduation_year INT,
+	college_name VARCHAR(32),
+	college_major VARCHAR(32),
+	min_req_salary INT,
+	FOREIGN KEY (candidateID) REFERENCES user(userID)
+);
