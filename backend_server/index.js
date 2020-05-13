d8dea9720a0bc3606f4c5d421058d0f8f72822f3
Pegah <pegsoltani@gmail.com>
2020-05-14 00:14:43 +0300

    Created the server

--- backend_server/index.js
+++ backend_server/index.js
@@ -1,4 +1,11 @@
+const mysql = require('mysql');
 const express = require('express'); // imports the library express
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "tryme_test_user",
+    password: "tOOl_1996_"
+}); // connecting my database to the server
 
 const app = express(); // calls a function express in express server application here
 
@@ -8,4 +15,8 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
     console.log('Start tryme!');
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
 }); // starts the server is the function that prints
