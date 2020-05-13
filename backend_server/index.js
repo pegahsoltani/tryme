c1e35727f550c35741ab9ed1b4c6bd3a1b4df10d
Pegah <pegsoltani@gmail.com>
2020-05-14 01:23:44 +0300

    added specific_user functionality

--- backend_server/index.js
+++ backend_server/index.js
@@ -4,7 +4,8 @@ const express = require('express'); // imports the library express
 const con = mysql.createConnection({
     host: "localhost",
     user: "tryme_test_user",
-    password: "tOOl_1996_"
+    password: "tOOl_1996_",
+    database: "tryme"
 }); // connecting my database to the server
 
 const app = express(); // calls a function express in express server application here
@@ -12,6 +13,25 @@ const app = express(); // calls a function express in express server application
 app.get('/', function (req, res) {
     res.send('Hell World.');
 }); // initializes the server and the methods
+
+// for each functionality create this with connection query
+app.get('/list_users', function (req, res) {
+    const query_string = "SELECT * FROM user";
+    con.query(query_string, function (err, result) {
+        if (err) throw err;
+        res.send(result);
+       // console.log("Result: " + result); // shows the message on terminal
+    }); // sending the quesries to the database
+});
+
+app.get('/specific_user', function (req, res) {
+    const query_string = "SELECT surname FROM user WHERE userID=?";
+    con.query(query_string, [req.query.userID],function (err, result) {
+        if (err) throw err;
+        res.send(result);
+        // console.log("Result: " + result); // shows the message on terminal
+    }); // sending the quesries to the database
+});
 
 app.listen(3000, function () {
     console.log('Start tryme!');
